ebab158afc0106e8354567bdf9d8503f36d0e29a
Anton K <i.openuser@yandex.ru>
2025-04-20 22:00:35 +0500

    slider

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = () => {
+	const swiper = new Swiper('.slider', {
+		// pagination: {
+		// 	el: '.swiper-pagination',
+		// },
+		navigation: {
+			nextEl: '.slider-button-next',
+			prevEl: '.slider-button-prev',
+		},
+		// effect: "fade",
+		// speed: 1000
+	})
+}
+slider()
