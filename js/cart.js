13eb82b0f5e2e7ad784deac3c385879c8170248b
Anton K <i.openuser@yandex.ru>
2025-04-20 17:05:51 +0500

    cart.js

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+const cartBtn = document.querySelector('.button-cart')
+const cart = document.getElementById('modal-cart')
+const cartClose = document.querySelector('.modal-close')
+
+cartBtn.addEventListener('click', function() {
+	cart.style.display = 'flex'
+})
+
+cartClose.addEventListener('click', function() {
+	cart.style.display = ''
+})
