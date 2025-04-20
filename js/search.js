c86e9b2c83a4a9e62f0341d24b12d11f53d1f166
Anton K <i.openuser@yandex.ru>
2025-04-20 19:16:43 +0500

    search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,13 @@
-const cartBtn = document.querySelector('.button-cart')
+const search = function() {
+	const input = document.querySelector('.search-block > input')
+	const searchBnt = document.querySelector('.search-block > button')
+	
+	input.addEventListener('input', (event) => {
+		console.log(event.target.value)
+	})
+
+	searchBnt.addEventListener('click', (event) => {
+		console.log(input.value)
+	})
+}
+search()
