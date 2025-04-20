915cdca335ed65723ec0e9ec19364dc73a006a5a
Anton K <i.openuser@yandex.ru>
2025-04-20 17:10:25 +0500

    encapsulation

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,14 @@
-const cartBtn = document.querySelector('.button-cart')
-const cart = document.getElementById('modal-cart')
-const cartClose = document.querySelector('.modal-close')
-
-cartBtn.addEventListener('click', function() {
-	cart.style.display = 'flex'
-})
-
-cartClose.addEventListener('click', function() {
-	cart.style.display = ''
-})
+const cart = function() {
+	const cartBtn = document.querySelector('.button-cart')
+	const cart = document.getElementById('modal-cart')
+	const cartClose = document.querySelector('.modal-close')
+	
+	cartBtn.addEventListener('click', function() {
+		cart.style.display = 'flex'
+	})
+	
+	cartClose.addEventListener('click', function() {
+		cart.style.display = ''
+	})
+}
+cart()
