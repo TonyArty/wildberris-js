e2fd8cc82c0c7ada8d4bfb929c9c1c74d90da798
Anton K <i.openuser@yandex.ru>
2025-04-21 12:59:24 +0500

    input.value

--- js/search.js
+++ js/search.js
@@ -1,12 +1,8 @@
 const search = function() {
 	const input = document.querySelector('.search-block > input')
 	const searchBnt = document.querySelector('.search-block > button')
-	
-	input.addEventListener('input', (event) => {
-		console.log(event.target.value)
-	})
 
-	searchBnt.addEventListener('click', (event) => {
+	searchBnt.addEventListener('click', () => {
 		console.log(input.value)
 	})
 }
